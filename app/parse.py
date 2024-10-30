c4869781ba79d634c03fe199ed78097785d12458
Olexandr Bastov <iamlucky1990@gmail.com>
2024-10-30 23:10:00 +0200

    solution

--- app/parse.py
+++ app/parse.py
@@ -1,4 +1,10 @@
+import csv
 from dataclasses import dataclass
+
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://quotes.toscrape.com"
 
 
 @dataclass
@@ -8,8 +14,38 @@ class Quote:
     tags: list[str]
 
 
+def get_single_quote(quote) -> Quote:
+    return Quote(
+        text=quote.find("span", itemprop="text").text,
+        author=quote.find("small").text,
+        tags=[tag.text for tag in quote.find_all("a", class_="tag")],
+    )
+
+
+def get_quotes() -> [Quote]:
+    session = requests.Session()
+    next_url = URL
+    all_quotes = []
+    while True:
+        soup = BeautifulSoup(session.get(next_url).text, "html.parser")
+        quotes = soup.find_all("div", class_="quote")
+        all_quotes.extend([get_single_quote(quote) for quote in quotes])
+        next_page = soup.find(class_="next")
+        if not next_page:
+            break
+        next_url = URL + next_page.find("a")["href"]
+    return all_quotes
+
+
 def main(output_csv_path: str) -> None:
-    pass
+    quotes = get_quotes()
+
+    with open(output_csv_path, mode="w", newline="", encoding="utf-8") as file:
+        writer = csv.writer(file)
+        writer.writerow(["text", "author", "tags"])
+
+        for quote in quotes:
+            writer.writerow([quote.text, quote.author, quote.tags])
 
 
 if __name__ == "__main__":
